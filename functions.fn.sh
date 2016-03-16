9a248166d29e3f3636c8b005c32579d8197feb26
Martin Hanzel <arthanzel@gmail.com>
2016-03-16 19:53:34 -0400

    Documentation

--- functions.fn.sh
+++ functions.fn.sh
@@ -1,3 +1,8 @@
+# functions.fn.sh
+# 
+# This file defines functions to be loaded before anything else. Put common
+# helpers and dependencies here.
+
 # addpath adds a path to the current $PATH and exports it.
 # If the path is invalid or already exists in the $PATH, it will be ignored.
 addpath() {
