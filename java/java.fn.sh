b70fb92dadd243185370f820e07958659b499132
Martin Hanzel <arthanzel@gmail.com>
2016-03-21 16:39:34 -0400

    fixed alternative tool, added java+gradle

--- java/java.fn.sh
+++ java/java.fn.sh
@@ -1,1 +1,5 @@
-export JAVA_HOME="/opt/java/jdk-current"
+export JAVA_HOME="/opt/java/java-current"
+addpath $JAVA_HOME/bin
+
+export GRADLE_HOME="/opt/gradle/gradle-current"
+addpath $GRADLE_HOME/bin
