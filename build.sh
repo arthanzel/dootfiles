57e928d4a6003b297c69d51b0d8aba635ed32a6f
Martin Hanzel <arthanzel@gmail.com>
2016-07-13 00:55:46 -0400

    Moved to bash-based build

--- build.sh
+++ build.sh
@@ -12,47 +12,75 @@ resolve() {
  	echo "$(cd "$(dirname "$1")" && pwd)/$(basename "$1")"
 }
 
-# For each *.link file in dootfiles
-for LINE in $(find . -name *.link); do
-	# This is the file to which the link points.
-	LINKTGT=$(resolve $LINE)
-	
-	# This is the bare filename of the linkable.
-	# E.g. shell/bashrc.link -> bashrc
-	NAME=$(basename $LINE | sed 's/\.link$//')
+# Install task
+# Use build.sh install force to force overwriting
+if [[ -z $1 || $1 == "install" ]]; then
+	echo "Installing dootfiles to home..."
 
-	# This is the actual file of the link.
-	LINKFILE=$(resolve ~)/.$NAME
-	
-	# If the link already exists, regardless of type.
-	# User can choose to skip, overwrite, or backup the existing file.
-	if [[ -e $LINKFILE ]]; then
-		echo "File already exists: ~/.$NAME"
+	# For each *.link file in dootfiles
+	for LINE in $(find . -name *.link); do
+		# This is the file to which the link points.
+		LINKTGT=$(resolve $LINE)
+		
+		# This is the bare filename of the linkable.
+		# E.g. shell/bashrc.link -> bashrc
+		NAME=$(basename $LINE | sed 's/\.link$//')
 
-		while true; do
-			printf "[s]kip, [o]verwrite, [b]ackup? "
-			read -n 1 CHAR
-			case $CHAR in
-				s)  echo skip
-					continue 2 # Break and continue on next linkable
-					;;
-				o)  echo overwrite
-					break
-					;;
-				b)  echo backup
-					mv $LINKFILE "${LINKFILE}.backup"
-					break
-					;;
-				*) echo default;;
-			esac
-		done
-	fi
+		# This is the actual file of the link.
+		LINKFILE=$(resolve ~)/.$NAME
 
-	# Link the file. The -f switch overwrites if $LINKFILE exists.
-	ln -sf $LINKTGT $LINKFILE
-done
+		# If the linkfile is a directory, warn the user and skip.
+		if [[ -d $LINKFILE ]]; then
+			echo "Warning: ~/.$NAME is a directory! Skipping."
+			continue
+		fi
+		
+		# If the linkfile exists and is a regular file.
+		# User can choose to skip, overwrite, or backup the existing file.
+		if [[ $2 != "force" && -f $LINKFILE ]]; then
+			echo "File already exists: ~/.$NAME"
 
-# Echo the quoted variable to preserve newlines. Not quoting replaces whitespace
-# with a single space.
-# Loop over every linkable and call process_linkable on it.
-# echo "$LINKABLES" | while read line; do process_linkable $line; done
+			while true; do
+				printf "[s]kip, [o]verwrite, [b]ackup? "
+				read -n 1 CHAR && echo
+				case $CHAR in
+					s)  continue 2 # Break and continue on next linkable
+						;;
+					o)  break
+						;;
+					b)  mv $LINKFILE "${LINKFILE}.backup"
+						break
+						;;
+				esac
+			done
+
+		fi
+
+		# Link the file. The -f switch overwrites if $LINKFILE exists.
+		ln -sf $LINKTGT $LINKFILE
+	done
+
+	echo "Done!"
+
+elif [[ $1 == "uninstall" ]]; then
+	echo "Removing dootfiles and restoring backups..."
+
+	for LINE in $(find . -name *.link); do
+		NAME=$(basename $LINE | sed 's/\.link$//')
+		LINKFILE=$(resolve ~)/.$NAME
+		BACKUPFILE="${LINKFILE}.backup"
+
+		# Check if the linkfile is a symlink and remove.
+		# TODO: Read the link destination and remove only if it points to dootfiles
+		if [[ -L $LINKFILE ]]; then
+			rm $LINKFILE
+		fi
+
+		# Restore the backup
+		if [[ -f $BACKUPFILE ]]; then
+			mv $BACKUPFILE $LINKFILE
+		fi
+	done
+
+	echo "Done!"
+fi
